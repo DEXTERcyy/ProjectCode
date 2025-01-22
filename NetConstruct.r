5979060dd56894ba37975f568e61cb47b1af8eaf
DEXTERcyy <dextercyy@gmail.com>
2025-01-22 21:09:20 +0100

    BigDataFiltered

--- NetConstruct.r
+++ NetConstruct.r
@@ -1,29 +1,4 @@
 # %%
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
-library(devtools, quietly = TRUE)
-library(BiocManager, quietly = TRUE)
-if (!requireNamespace("phyloseq", quietly = TRUE)) BiocManager::install("phyloseq")
-if (!requireNamespace("SPRING", quietly = TRUE)) devtools::install_github("GraceYoon/SPRING")
-if (!requireNamespace("SpiecEasi", quietly = TRUE)) devtools::install_github("zdk123/SpiecEasi")
-if (!requireNamespace("stabJGL", quietly = TRUE)) devtools::install_github("camiling/stabJGL")
-devtools::install_github("stefpeschel/NetCoMi", ref = "develop",repos = c("https://cloud.r-project.org/",BiocManager::repositories()))
-if (!requireNamespace("caret", quietly = TRUE)) install.packages("caret")
-if (!requireNamespace("pROC", quietly = TRUE)) install.packages("pROC")
-if (!requireNamespace("MLmetrics", quietly = TRUE)) install.packages("MLmetrics")
-if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
-if (!requireNamespace("tidyr", quietly = TRUE)) install.packages("tidyr")
-if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr") # Likely installed with tidyverse
-if (!requireNamespace("ggraph", quietly = TRUE)) install.packages("ggraph")
-if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse") # Installs a suite of packages
-if (!requireNamespace("RColorBrewer", quietly = TRUE)) install.packages("RColorBrewer")
-if (!requireNamespace("foreach", quietly = TRUE)) install.packages("foreach")
-if (!requireNamespace("doParallel", quietly = TRUE)) install.packages("doParallel")
-if (!requireNamespace("parallel", quietly = TRUE)) install.packages("parallel") # Usually base R, no need to install
-if (!requireNamespace("mixedCCA", quietly = TRUE)) install.packages("mixedCCA")
-if (!requireNamespace("igraph", quietly = TRUE)) install.packages("igraph")
-if (!requireNamespace("qgraph", quietly = TRUE)) install.packages("qgraph")
-
 library(phyloseq)
 library(SPRING)
 library(SpiecEasi)
@@ -38,43 +13,63 @@ library(RColorBrewer)
 library(dplyr)
 source("Packages\\stabENG.r")
 source("Packages\\MyENG.r")
-
-rawdata <- readRDS("data\\n_starvation.rds")
-otu_Ab <- as.data.frame(t(otu_table(rawdata)))
+source("Packages\\stabENG.r")
+rawdata <- readRDS("data\\DavarData1_substrate_phyloseq_1226_final_filtered.RDS")
+otu_raw <- otu_table(rawdata)
+otu_RA <- transform_sample_counts(otu_raw, function(x) x / sum(x) )
+otu_RA <- filter_taxa(otu_RA, function(x) mean(x) > 1e-3, TRUE)
+shared_otu <- rownames(otu_RA)
+otu_Ab <- as.data.frame(t(otu_raw[shared_otu,]))
 sam_info <- as.data.frame(sample_data(rawdata))
 otu_tax <- as.data.frame(tax_table(rawdata))
-shared_otu <- colnames(otu_Ab)
-# split otu_Ab by meta
+
+# %%split otu_Ab by condition group
 otu_Ab_Nplus <- otu_Ab[rownames(otu_Ab) %in% rownames(sam_info[sam_info$growthCondition=="plusN",]),]
 otu_Ab_Nminus <- otu_Ab[rownames(otu_Ab) %in% rownames(sam_info[sam_info$growthCondition=="minusN",]),]
 data_list <- list(Nplus = otu_Ab_Nplus, Nminus = otu_Ab_Nminus)
-
-network_results <- stabENG(data_list, labels = shared_otu, var.thresh = 0.1, rep.num = 20,
+# by Group and Days
+timestamps <- unique(sam_info$Days)
+otu_Ab_Nplus_times <- list()
+otu_Ab_Nminus_times <- list()
+data_list_times <- list()
+for (i in timestamps)
+  {
+    otu_Ab_Nplus_times[[i]] <- otu_Ab[rownames(otu_Ab) %in% 
+      rownames(sam_info[sam_info$growthCondition=="plusN" & sam_info$Days == i,]),]
+    otu_Ab_Nminus_times[[i]] <- otu_Ab[rownames(otu_Ab) %in%
+      rownames(sam_info[sam_info$growthCondition=="minusN" & sam_info$Days == i,]),]
+    data_list_times[[i]] <- list(Nplus = otu_Ab_Nplus_times[[i]], Nminus = otu_Ab_Nminus_times[[i]])
+  }
+# %%
+i = "12"
+data_list <- data_list_times[[i]]
+cat('Calculating network on day ',i,'\n')
+network_results <- stabENG(data_list, labels = shared_otu, var.thresh = 0.1, rep.num = 25,
   nlambda1=20,lambda1.min=0.01,lambda1.max=1,nlambda2=20,lambda2.min=0,lambda2.max=0.1,
-  lambda2.init=0.01,ebic.gamma=0.2, parallelize=T)
+  lambda2.init=0.01,ebic.gamma=0.6)
 network_Nplus <- network_results$opt.fit$Nplus # precision matrix estimates
 network_Nminus <- network_results$opt.fit$Nminus # precision matrix estimates
+# filter edge sparsity
+network_Nplus[abs(network_Nplus) < 0.1] <- 0
+network_Nminus[abs(network_Nminus) < 0.1] <- 0
 diag(network_Nplus) = diag(network_Nminus) <- 0
-network_Nplus_pcor <- network_results$opt.fit.pcor$Nplus
-network_Nminus_pcor <- network_results$opt.fit.pcor$Nminus
-#save.image("network_results_stabENG.RData")
-#load(file = "network_results_stabENG.RData")
-
 # %% Plot network on Phylum level
 Phylum_groups <- as.factor(otu_tax[rownames(network_Nplus),"Phylum"])
-png(filename="Plots/network_Nplus_Phylum_Stab.png")
+png(filename=paste0("Plots/BigDataFilter/Days_",i,"_Big_Network_Nplus_Phylum_Stab_Filtered_vsized.png"))
 qgraph::qgraph(network_Nplus, 
   layout = "circle",
   edge.color = ifelse(network_Nplus > 0, "blue", "red"),
   title = "Stab Network Nplus by Phylum",
+  vsize = 2.5,
   groups = Phylum_groups)
 dev.off()
-
-png(filename="Plots/network_Nminus_Phylum_Stab.png")
+# %%
+png(filename=paste0("Plots/BigDataFilter/Days_",i,"_Big_Network_Nminus_Phylum_Stab_Filtered_vsized.png"))
 qgraph::qgraph(network_Nminus, 
   layout = "circle",
   edge.color = ifelse(network_Nminus > 0, "blue", "red"),
   title = "Stab Network Nminus by Phylum",
+  vsize = 2.5,
   groups = Phylum_groups)
 dev.off()
 
@@ -136,16 +131,16 @@ ggraph::ggraph(mygraph, layout = 'dendrogram', circular = TRUE) +
   geom_node_point(aes(filter = leaf, x = x*1.07, y=y*1.07, colour=group, size=value, alpha=0.2)) +
   scale_colour_manual(values= rep( brewer.pal(14,"Paired") , 30)) +
   scale_size_continuous(range = c(0.1,10) ) +
-
+# set width by edge cor value?
   theme_void() +
   theme(
     legend.position="none",
     plot.margin=unit(c(0,0,0,0),"cm"),
   ) +
   expand_limits(x = c(-1.3, 1.3), y = c(-1.3, 1.3))
-ggsave(filename="Plots/Nplus_plot_circularized.pdf", width = 12, height = 12, units = "in")
+ggsave(filename=paste0("Plots/BigDataFilter/Days_",i,"_Big_Nplus_Filtered_plot_circularized.pdf"), width = 12, height = 12, units = "in")
 
-# Visualize Edge weights
+# %%Visualize Edge weights
 cor_values_Nplus <- as.vector(network_Nplus)
 cor_values_Nminus <- as.vector(network_Nminus)
 cor_df <- data.frame(
@@ -157,9 +152,9 @@ ggplot2::ggplot(cor_df, aes(x = correlation, fill = group)) +
   scale_fill_manual(values = c("Nplus" = "blue", "Nminus" = "red")) +
   theme_minimal() +
   labs(title = "Distribution of Correlation Values",
-       x = "Correlation",
-       y = "Frequency",
-       fill = "Nitro Condition")
+      x = "Correlation",
+      y = "Frequency",
+      fill = "Nitro Condition")
 
 #%%
 set.seed(10010)
@@ -172,26 +167,33 @@ synthesize_scaled_data <- function(dat, net)
     X <- SpiecEasi::synth_comm_from_counts(dat, mar = 2, distr = 'zinegbin', Sigma = Cor, n = nrow(dat))
     return(X)
   }
-
+cat('Synthesize simulation data on day ',i,'\n')
 Sim_list <- list()
-for (i in 1:50)
+for (j in 1:5)
   {
-    Sim_list[[i]] <- list(
+    Sim_list[[j]] <- list(
       Nplus = synthesize_scaled_data(otu_Ab_Nplus, network_Nplus),
       Nminus = synthesize_scaled_data(otu_Ab_Nminus, network_Nminus)
     )
   }
+cat('Calculate simulation data network on day ',i,'\n')
 Res_sim <- list()
-for (i in 1:50)
+for (j in 1:5)
   {
-    Res_sim[[i]] <- stabENG(Sim_list[[i]], labels = shared_otu)
+    Res_sim[[j]] <- stabENG(Sim_list[[j]], labels = shared_otu, var.thresh = 0.1, rep.num = 25,
+      nlambda1=20,lambda1.min=0.01,lambda1.max=1,nlambda2=20,lambda2.min=0,lambda2.max=0.1,
+      lambda2.init=0.01,ebic.gamma=0.6)
+    # filter edge sparsity
+    Res_sim[[j]]$opt.fit$Nplus[abs(Res_sim[[j]]$opt.fit$Nplus) < 0.01] <- 0
+    Res_sim[[j]]$opt.fit$Nminus[abs(Res_sim[[j]]$opt.fit$Nminus) < 0.01] <- 0
+    diag(Res_sim[[j]]$opt.fit$Nplus) = diag(Res_sim[[j]]$opt.fit$Nminus) <- 0
   }
 Sim_adj <- list()
-for (i in 1:50)
+for (j in 1:5)
   {
-    Sim_adj[[i]] <- list(
-      Nplus = (Res_sim[[i]]$opt.fit$Nplus !=0)*1,
-      Nminus = (Res_sim[[i]]$opt.fit$Nminus !=0)*1
+    Sim_adj[[j]] <- list(
+      Nplus = (Res_sim[[j]]$opt.fit$Nplus !=0)*1,
+      Nminus = (Res_sim[[j]]$opt.fit$Nminus !=0)*1
     )
   }
 #%% Confusion matrices
@@ -216,8 +218,8 @@ calculate_metrics <- function(true_adj, sim_adj)
     # Confusion matrix
     cm <- confusionMatrix(as.factor(sim_edges), as.factor(true_edges), positive = "1")
     tn <- as.numeric(cm$table[1,1]) #true negatives
-    fp <- as.numeric(cm$table[1,2]) #false positives
-    fn <- as.numeric(cm$table[2,1]) #false negatives
+    fn <- as.numeric(cm$table[1,2]) #false negatives
+    fp <- as.numeric(cm$table[2,1]) #false positives
     tp <- as.numeric(cm$table[2,2]) #true positives
   
     # Calculate TPR, FPR, Precision, Recall
@@ -227,7 +229,7 @@ calculate_metrics <- function(true_adj, sim_adj)
     recall <- tpr
   
     # Calculate ROC and AUC
-    roc_obj <- roc(true_edges, sim_edges)
+    roc_obj <- pROC::roc(true_edges, sim_edges)
     auc <- as.numeric(auc(roc_obj)) # plot here
   
     # F1 Score
@@ -240,13 +242,13 @@ calculate_metrics <- function(true_adj, sim_adj)
     return(list(TPR = tpr, FPR = fpr, Precision = precision, Recall = recall,
                 F1 = f1, AUC = auc, MCC = mcc))
   }
-
-confusion_results <- lapply(1:50, function(i)
+cat('Calculate confusion matrices on day ',i,'\n')
+confusion_results <- lapply(1:5, function(j)
   {
     true_adj_Nplus <- (network_Nplus !=0)*1
     true_adj_Nminus <- (network_Nminus !=0)*1
-    Nplus_metrics <- calculate_metrics(true_adj_Nplus, Sim_adj[[i]]$Nplus)
-    Nminus_metrics <- calculate_metrics(true_adj_Nminus, Sim_adj[[i]]$Nminus)
+    Nplus_metrics <- calculate_metrics(true_adj_Nplus, Sim_adj[[j]]$Nplus)
+    Nminus_metrics <- calculate_metrics(true_adj_Nminus, Sim_adj[[j]]$Nminus)
     return(list(Nplus = Nplus_metrics, Nminus = Nminus_metrics))
 })
 
@@ -256,10 +258,10 @@ results_df <- do.call(rbind, lapply(confusion_results, as.data.frame))
 results_df_long <- results_df %>%
   dplyr::select(starts_with("Nplus.") | starts_with("Nminus.")) %>%
   tidyr::pivot_longer(cols = everything(), 
-               names_to = c("group", "metric"), 
-               names_sep = "\\.",
-               values_to = "value") %>%
-  dplyr::mutate(matrix_id = rep(1:50, each = 14))
+              names_to = c("group", "metric"), 
+              names_sep = "\\.",
+              values_to = "value") %>%
+  dplyr::mutate(matrix_id = rep(1:5, each = 14))
 
 # %% barplot
 for (metric_name in unique(results_df_long$metric)) {
@@ -271,7 +273,7 @@ for (metric_name in unique(results_df_long$metric)) {
       x = "Matrix ID",
       y = metric_name) +
     theme_bw()
-    ggsave(filename = paste0("Plots/", metric_name, "_barplot.png"), p)
+    ggsave(filename = paste0("Plots/BigDataFilter/Days_Big_Filtered_",i,"_", metric_name, "_barplot.png"), p)
 }
 
 # %% boxplot
@@ -281,8 +283,8 @@ for (metric_name in unique(results_df_long$metric)) {
   p <- ggplot(plot_data, aes(x = group, y = value, fill = group)) +
     geom_boxplot() +
     labs(title = paste(metric_name, "Distribution"),
-         x = "Nitrogen Condition",
-         y = metric_name) +
+        x = "Nitrogen Condition",
+        y = metric_name) +
     theme_bw()
-  ggsave(filename = paste0("Plots/", metric_name, "_boxplot.png"), p)
+  ggsave(filename = paste0("Plots/BigDataFilter/Days_Big_Filtered_",i,"_", metric_name, "_boxplot.png"), p)
 }
